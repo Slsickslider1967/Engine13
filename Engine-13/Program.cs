81454832666c194b08dea75af8be7c63fbc85382
Slsickslider <georgiorocco@outlook.com>
2025-09-24 22:38:29 +0100

    Testing Engine.cs on laptop

--- Engine-13/Program.cs
+++ Engine-13/Program.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using Veldrid;
+﻿using Veldrid;
 using Veldrid.Sdl2;
 using Veldrid.StartupUtilities;
-using Vortice.Mathematics;
+using Engine13.Core;
+using System;
 
 class Program
 {
@@ -19,56 +18,21 @@ class Program
         };
 
         Sdl2Window Window = VeldridStartup.CreateWindow(Window_Create_Info);
-
-        GraphicsBackend BakcEnd = GraphicsBackend.Vulkan;
-
+        GraphicsBackend Backend = GraphicsBackend.Vulkan;
         GraphicsDeviceOptions GD_Options = new GraphicsDeviceOptions
         {
             Debug = true,
             SyncToVerticalBlank = true,
             SwapchainDepthFormat = PixelFormat.R16_UNorm
         };
+        //Swapchain SC = GD_Options.MainSwapchain;
 
-        Engine _engine = new Engine(Window, GD_Options, BakcEnd);
+        Engine _engine = new Engine(Window, GD_Options, Backend);
         _engine.Run();
 
-        GraphicsDevice GD = VeldridStartup.CreateGraphicsDevice(Window, GD_Options, BakcEnd);
-        Swapchain SC = GD.MainSwapchain;
-        CommandList Cl = GD.ResourceFactory.CreateCommandList();
+        // GraphicsDevice GD = VeldridStartup.CreateGraphicsDevice(Window, GD_Options, Backend);
+        //
+        // CommandList Cl = GD.ResourceFactory.CreateCommandList();
 
-        // Example RGB values (0-255)
-        byte R = 0, G = 0, B = 0; // DodgerBlue
-        RgbaFloat clearColor = new RgbaFloat(R / 255f, G / 255f, B / 255f, 1f);
-
-        while (Window.Exists)
-        {
-            Window.PumpEvents();
-            if (!Window.Exists) break;
-
-             if (Window.Width != SC.Framebuffer.Width || Window.Height != SC.Framebuffer.Height)
-             {
-                 GD.ResizeMainWindow((uint)Window.Width, (uint)Window.Height);
-                 SC = GD.MainSwapchain;
-             }
-
-            R += 1;
-            if (R >= 255) G += 1;
-            if (G >= 255) B += 1;
-            Console.WriteLine($"R: {R}, G: {G}, B: {B}");
-            clearColor = new RgbaFloat(R / 255f, G / 255f, B / 255f, 1f);
-
-            // Here you would typically call your engine's run method
-            // For example: engine.Run();
-
-            Cl.Begin();
-            Cl.SetFramebuffer(GD.SwapchainFramebuffer);
-            Cl.ClearColorTarget(0, clearColor);
-            Cl.End();
-
-            GD.SubmitCommands(Cl);
-            GD.SwapBuffers();
-        }
-
-        GD.Dispose();
     }
 }
