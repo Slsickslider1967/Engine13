e67baa10b32947f82f1f052bbdc60a14c31e99c3
Slsickslider <georgiorocco@outlook.com>
2025-09-25 13:53:52 +0100

    Working on rectangle

---
+++ Graphics/Mesh.cs
@@ -0,0 +1,76 @@
+using Veldrid;
+using System;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+using Vulkan;
+
+namespace Engine13.Graphics
+{
+    public struct VertexPosition
+    {
+        public const uint SizeInBytes = 8; // 2 floats (X, Y) * 4 bytes each
+
+        public float X;
+        public float Y;
+
+        public VertexPosition(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class Mesh
+    {
+        // Declare class members
+        private VertexPosition[] vertices;
+        private ushort[] indices;
+
+        public DeviceBuffer VertexBuffer { get; private set; }
+        public DeviceBuffer IndexBuffer { get; private set; }
+        
+        public Mesh(GraphicsDevice GD, VertexPosition[] vertices, ushort[] indices)
+        {
+            this.vertices = vertices;
+            this.indices = indices;
+
+            // Create vertex buffer
+            DeviceBuffer VertexBuffer = GD.ResourceFactory.CreateBuffer(new BufferDescription(
+                (uint)(vertices.Length * VertexPosition.SizeInBytes),
+                BufferUsage.VertexBuffer));
+
+            // Upload vertex data
+            GD.UpdateBuffer(VertexBuffer, 0, vertices);
+
+            // Create index buffer
+            DeviceBuffer IndexBuffer = GD.ResourceFactory.CreateBuffer(new BufferDescription(
+                (uint)(indices.Length * sizeof(ushort)),
+                BufferUsage.IndexBuffer));
+
+            // Upload index data
+            GD.UpdateBuffer(IndexBuffer, 0, indices);
+        }
+
+        public static Mesh CreateQuad(GraphicsDevice GD, float Width, float Height)
+        {
+            VertexPosition[] vertices = new VertexPosition[]
+            {
+                new VertexPosition(-0.5f * Width, -0.5f * Height), // Bottom-left
+                new VertexPosition(0.5f * Width, -0.5f * Height), // Bottom-right
+                new VertexPosition(-0.5f * Width, 0.5f * Height), // Top-right
+                new VertexPosition(0.5f * Width, 0.5f * Height)  // Top-left
+            };
+
+            ushort[] indices =
+            {
+                0, 1, 2,
+                1, 3, 2
+            };
+
+            return new Mesh(GD, vertices, indices);
+
+        }
+    }
+
+
+}
